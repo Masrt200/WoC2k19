4101cab24991a6310582bd7fccfe88050a1fcc47
Masrt200 <immasrt@gmail.com>
2019-12-18 02:26:02 +0530

    rsa related

---
+++ Ext_Euclid.py
@@ -0,0 +1,35 @@
+#extended Euclid
+
+a=int(input("Enter 1st no.:"))
+b=int(input("Enter 2nd no.:"))
+if a>b:   #reversing values of a and b
+    a=a+b
+    b=a-2*b
+    a=(a-b)//2
+    b=a+b
+
+c=a   #calling vals of a and c for calcing
+d=b
+
+uc=1  #these vars are used for maintaining the 'if' condition
+vc=0
+ud=0
+vd=1
+
+while c!=0:
+    if uc*a+vc*b==c and ud*a+vd*b==d:  #main invariant condition
+        q=d//c
+        m=c  #storing c
+        c=d-q*c
+        d=m
+        
+        Uc=uc #storing uc and vc
+        Vc=vc
+        
+        uc=ud-q*uc
+        vc=vd-q*vc
+        ud=Uc
+        vd=Vc
+
+
+print(str(ud)+"*"+str(a)+"+"+str(vd)+"*"+str(b)+"="+str(d))
--- rsa.py
+++ rsa.py
@@ -1,10 +1,116 @@
 import math
-import random
-#encryption
-a=input("Enter padded message 'm':")
-#choosing private and public keys
-l=input("Select prime no. limit (100-1000):")
-A[]
-for i in l
-A[i-1]=i
-print A
+
+def fac(no):
+    pri=math.remainder(no,6)
+    if pri==1 or pri==-1 :  #checks 6n-1 and 6n+1 condition for primes
+        if no%5 !=0:
+            if n%no==0 :
+                P[0]+=str(no)
+                return 1
+            
+    if no==2 or no==3 or no==5:
+        if n%no==0:
+            P[0]+=str(no)
+            return 1
+
+def check_fac(a,b):
+    while a<b:
+        fd=fac(a)
+        if fd==1:
+            return 1
+            break
+        a+=1
+
+def ext_Euclid(A,B):
+    cn=0
+    if A>B:  #reversing vals in case e>totient
+        A=A+B
+        B=A-2*B
+        A=(A-B)//2
+        B=A+B
+        cn+=1
+
+    c=A   #calling vals of a and c for calcing
+    d=B
+    
+    uc=1  #these vars are used for maintaining the 'if' condition
+    vc=0
+    ud=0
+    vd=1
+
+    while c!=0:
+        if uc*A+vc*B==c and ud*A+vd*B==d:  #main invariant condition
+            q=d//c
+            m=c  #storing c
+            c=d-q*c
+            d=m
+        
+            Uc=uc #storing uc and vc
+            Vc=vc
+        
+            uc=ud-q*uc
+            vc=vd-q*vc
+            ud=Uc
+            vd=Vc
+    if cn==0:
+        if ud<0:
+            ud=B+ud
+        return ud
+    else:
+        if vd<0:
+            vd=vd+A
+        return vd
+
+P=[''] #for storing value of p        
+n=int(input("Enter value of public key n:"))   #n=p*q, we have to factorize n for p and q
+#if n is not a perfect square which it isn't since p and q are distinct primes
+#it is known that p<(n)^0.5<q if p<q... which means on of the prime factors of n
+#is less than its square root... also it reduces the range of search by a considerable
+#amount as n increases
+
+fd=0
+t=0
+r2=math.isqrt(n)               #square root
+r3=math.floor(math.pow(n,1/3)) #cube root
+r4=math.floor(math.pow(n,1/4)) #fourth root
+r5=math.floor(math.pow(n,2/5)) #under root of 2.5
+
+
+a=r5    #initialization
+b=r2+1  #if someone if stupid enough for n=p*p
+ct=1 #counts how my blocks of search have been performed(B1= r5-r2;B2= r3-r5;
+#B3= r4-r3;B4= 0-r4)
+while True:
+    t=check_fac(a,b)
+    if t==1:
+        break
+    
+    ct+=1
+    if ct==2:
+        a=r3
+        b=r5
+    elif ct==3:
+        a=r4
+        b=r3
+    elif ct==4:
+        a=2
+        b=r4
+    else:
+        break
+
+p=int(P[0])
+q=int(n/p)
+print("p:"+str(p))
+print("q:"+str(q))
+tot=(p-1)*(q-1)
+print("Totient:"+str(tot))
+e=int(input("Enter value of public key e:"))
+d=ext_Euclid(e,tot)
+print("Private key d:"+str(d))
+input()
+    
+
+
+#try making the blocks more based on prob as the value increases
+#remove i/p and o/p errors (currently prints 1 for 1,2,3,4,5,7,11,13)
+#figure out extended euclidean
