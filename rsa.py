319cec81cb99904efad626fc012a82713db0ab96
Masrt200 <immasrt@gmail.com>
2019-12-23 03:20:08 +0530

    all done except https reqs

--- rsa.py
+++ rsa.py
@@ -1,5 +1,5 @@
 import math
-
+import binascii
 def fac(no):
     pri=math.remainder(no,6)
     if pri==1 or pri==-1 :  #checks 6n-1 and 6n+1 condition for primes
@@ -21,7 +21,7 @@ def check_fac(a,b):
             break
         a+=1
 
-def ext_Euclid(A,B):
+def ext_Euclid(A,B):  #for calcing value 'd' by extended Euclidean Algorithm
     cn=0
     if A>B:  #reversing vals in case e>totient
         A=A+B
@@ -30,7 +30,7 @@ def ext_Euclid(A,B):
         B=A+B
         cn+=1
 
-    c=A   #calling vals of a and c for calcing
+    c=A   #calling vals of A and B for calcing
     d=B
     
     uc=1  #these vars are used for maintaining the 'if' condition
@@ -52,6 +52,7 @@ def ext_Euclid(A,B):
             vc=vd-q*vc
             ud=Uc
             vd=Vc
+            
     if cn==0:
         if ud<0:
             ud=B+ud
@@ -64,7 +65,7 @@ def ext_Euclid(A,B):
 P=[''] #for storing value of p        
 n=int(input("Enter value of public key n:"))   #n=p*q, we have to factorize n for p and q
 #if n is not a perfect square which it isn't since p and q are distinct primes
-#it is known that p<(n)^0.5<q if p<q... which means on of the prime factors of n
+#it is known that p<(n)^0.5<q if p<q... which means one of the prime factors of n
 #is less than its square root... also it reduces the range of search by a considerable
 #amount as n increases
 
@@ -107,6 +108,15 @@ print("Totient:"+str(tot))
 e=int(input("Enter value of public key e:"))
 d=ext_Euclid(e,tot)
 print("Private key d:"+str(d))
+
+C=int(input("Enter cipher text c:"))
+
+m=pow(C,d,n)  #using property of pow for calcing m=(c**d)%n
+print("Decrypted text:"+str(m))
+
+message=binascii.unhexlify(hex(m)[2:]).decode()
+print("Decrypted message:"+str(message))
+
 input()
     
 
@@ -114,3 +124,6 @@ input()
 #try making the blocks more based on prob as the value increases
 #remove i/p and o/p errors (currently prints 1 for 1,2,3,4,5,7,11,13)
 #figure out extended euclidean
+
+#padding... test-->bytes-->hex-->int of hex obtained
+#reverse message obtained: int obatined-->hex-->bytes-->string
