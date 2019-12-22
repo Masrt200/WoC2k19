f056c0a283d3fc284e4b23a7397887d24a544021
Masrt200 <immasrt@gmail.com>
2019-12-23 03:20:35 +0530

    rsa related

---
+++ rsa_encryptor.py
@@ -0,0 +1,21 @@
+#rsa encrypter
+import binascii
+
+message=input("Enter the message to be encrypted:")
+
+byte=message.encode()
+hexdump=binascii.hexlify(byte)  #used for giving hex val of binary data
+print("Hex'd Message:",hexdump)
+
+plaintext=int(hexdump,16)       #plaintext to be encrypted
+print("Plain Text:",plaintext)
+
+n=int(input("Enter the public key n:"))
+if plaintext>n:
+    raise Exception("Plaintext is too large for given key")
+#plaintext must always be less than n...
+
+e=int(input("Enter the public key e:"))
+
+ciphertext=pow(plaintext,e,n)   #as per rsa encryption (c**e)%n
+print("Cipher Text:",ciphertext)
