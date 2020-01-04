766c2b43f81aa996beabd7ff4ae7dc30022b91a9
Masrt200 <immasrt@gmail.com>
2020-01-04 15:00:07 +0530

    Updated Vigenere and failed tries

--- Vigenere.py
+++ Vigenere.py
@@ -1,88 +1,28 @@
-import os
-from time import sleep
-def clear():
-    os.system('cls')
+def vignere_enc(plaintext,key):
+	ciphertext=''
+	alphabet='abcdefghijklmnopqrstuvwxyz'
 
-while True:
-    td=input("What to do encrypt/decrypt?(e/d):")
-    Td=td.casefold()
-    if Td=='d' :
-        a=input("Enter Vigenere Cipher:")
-        break
-    elif Td=='e' :
-        a=input("Enter Plain Text:")
-        break
-    else :
-        print("Plz input correctly!")
-        sleep(1)
-        clear()
-    
-Cy=list(a)
-Dy=[]
+	i=0
+	while i<len(plaintext):
+		ct=0
+		enc=plaintext[i]
+		if enc.isupper():
+			enc=enc.lower()
+			ct+=1
 
-while True :   #checking for key format
-    b=input("Enter Key:")
-    Ky=list(b)
-    k=0
-    while k<len(Ky) :
-        p=ord(Ky[k])
-        if p>=65 and p<=90 :
-            pass
-        elif p>=97 and p<=122 :
-            pass
-        else :
-            print("The key should contain only letters")
-            sleep(1)
-            clear()
-            break
-        k+=1
+		if enc in alphabet:
+			encry=(ord(enc)+ord(key[i%len(key)])-194)%26
 
-    if k==len(Ky) :
-        break
+			char=chr(encry+97)
+			if ct==1:
+				char=char.upper()
 
-def decipher(c,j,kl) :
-    d=(ord(Cy[j])-kl)%26 #character code for decrypted letter
-    Dy.append(chr(d+c))
+			ciphertext+=char
+		else:
+			ciphertext+=enc
 
-def encipher(c,j,kl) :
-    d=(ord(Cy[j])+kl-2*c)%26 #character code for encrypted letter
-    Dy.append(chr(d+c))
+		i+=1
 
-if Td=='e' :
-    func=encipher
-elif Td=='d' :
-    func=decipher
+	print("Ciphertext:",ciphertext)
 
 
-i=0
-while i<len(Cy) :  #Finding the encr or decr text
-    m=ord(Cy[i])
-    lk=ord(Ky[i%len(Ky)])
-    if m>=65 and m<=90 :
-        
-        if Ky[i%len(Ky)].islower()==True : #for converting lower case letters in  key to match with upper case ones
-            lk=lk-32
-            
-        func(65,i,lk)
-    elif m>=97 and m<=122 :
-        
-        if Ky[i%len(Ky)].isupper()==True : #vice-versa
-            lk=lk+32
-            
-        func(97,i,lk)
-    else :
-        Dy.append(Cy[i])
-    i+=1
-
-b=''
-if Td=='e' :
-    print("Encrypted Cipher:"+b.join(Dy))
-elif Td=='d' :
-    print("Decrypted Soln:"+b.join(Dy))
-
-input()
-
-
-    
-    
-        
---
+++ vignere_so_hard.py
@@ -0,0 +1,50 @@
+#vignere_cip2.py
+import operator
+
+assume=4
+
+ciphertext="Jb Zpyéwpb - W Micccf Myv!, Bgv csuko vst xyvfboz od hvo ouo ct 10 kt o xofw-vskshsn obn ksvm-wxusbdjcboe peu gdvppysb, sogr, oxe owbhses Dyléayo Hbbwbos. Rf kkt tysqsn hc bqqoqh k ghecpcbo Dsloqrv tbpa Zsctotgcb Cou og iwg thobusf Qcyéncb csqkvgs is gpys vd vbhs pb dis nbm rf kkt gou hy usd vwc twbth Zpyéwpb. Is gbg nfhsbnwboe hy oqrjsjo vwc uckm oxe krfb Zjyomii cbk dioh is gbg gjzzsou dp qyndfynwgo vwc gopfhm uc ufsd jh cbts, hvo hky ct uvsw tcbnsr bb oyhfonszi ghbpbu ccbn hvku vkt pofb xphwmfr kor bfaoblsr vdcx pm nobi qvksoqdffg jb dis kowao. Hrjg eopfobyolms pswsxegvsq kyvzr tsh iwg dcibts ppf dis pvhibf. So Dyléayo Swffuooqm!, Asthm uczn ctp Ogr otdff cis ppibn vsb pwuf Oci vke pysfcgfr dp gkws Zjyomii gjhv xog xfsmlsr. Oh uvs fbr pt dis oqwgyes, Usow Fcmlsh noro wh uvsss dbjaobz clksqdjjs uc cusov Dwubqve otdff gjhbotgwxh Dsloqrv'g qckos. Wjghi qcxuwbefr ppzzyxwbq Ogr og is rforoe tys Doxhsb Qwdz, dfzzsou rja so Grpkryxb so Doxhsb Qwdz hrbh cis gpizn qcxuwbef tymzcgjbu iwa vbhsm grf kkt dkjr lbqy gcf uvs cwyo. Oci qkvuvd vwc twbth Zpyéwpb, b Qkusfzjs, jb Ktv Mbhqrfg k Dcuéacx. Wx Gvyxrcgo wx Dsgusf Dwhi, Oci rodwroe hy qcwqsho wb uvs Qcyéncb Msoqvs bfuwyooz dvowqwcxtvwzt pi sobowbq Ponhsg gfcw Umw Zskesfc oqbpgg uvs Lobdp fohwcx. Oci tyvuvd Pfydy, uvs Qskdff Qza Vforos, so o Qcyéncb cohdms kor vpgh fogsmm. Eifsou rjg bfaoddv, Bgv osobmm nftskusr Cfcml peu fogigoe hy uwff hrf tsooz czcg rio hc uvs jbhosjsxuwcx ct Cfcml'g zcixhsf twpvjbuc. Hyvqvoe pi Ogr'g ujbrxfgg uc Zpyéwpb kor gjhv uvs sshesb yg vst scufoxhsr gohrff, Gzwxu, Lscqu rsmjrsn hc hc gjhv Bgv bbr qifcvs rjg nssow ct cswxh hrf kyszr't poth Zpyéwpb Lsssnff kor qbjs Bgv uvs Ccivesf Corqf. So Hrf Kkusf Gzcgffg pt Mffivfob Dwhi, Oci fobqvoe hrf Qosizobb Qza kor gbg cicqufr dp zobfb uvod Awcum gbg yos yg wdt Uin Zobrsbt. Diciqi hrf akuqv xog jbhosfizusr xvsx Hskn Fydysd hfsfr dp gdfoz uvs Qcyéncb gfcw hvo Umw, Asthm't gsthsbt ogbfroe Oci o Dogmbrs Corqf krfb Zjyomii cbjsn hvo Umw kwdi o Uvixesflpzh. Vs dcbdjbioe vst xyvfboz ksuv Wjghi, krp vke rbfoac ct csqynwbq hvo kcbmr'c ufobhscu Kkusf Qcyéncb Nogdff. Jb Miofwbbros – Uvs Thfkz Dyléayo, Ktv kor pswsxeg njgqywsfoe o Dvobnobnff dioh ior cssx zspu wx hvo foso pi whc tcbnsf ufososf, Rowjob. Pfydy mbffsfr Miofwbbros hy hvo Dcuéacx Qsxusf, kvsms Ktv rfzr iwg dcod cjos hrf Qrbfakorsb'g dbwz gzowf hy ysoq wd zwd. Opusf Dvobnobnff cbjsn Dwubqve tfyn pojbu uoyoo pi Hskn Fydysd, Oci qkvuvd wh. Sjosm djas is diciqih rf kkt ryjbu xszv, Oci kyvzr tcaoick dfcct dkuvg xwhr vwc qvsmrvypr bjjov, Uksm. Hofi obn Dfygsgcpf Yby bfawxesr Bgv uvod vs xog bzkkzg k ghoq poiwbn Uobz. So Aithsbz od hvo Zwqihvyvgs, Dfygsgcpf Yby dpzr Bgv uvod Uobz vke qkvuvd 45 Qcyéncb bh k dcsoh gisb Bgv pbzi von gsffb. Jb Stzoxe cp hvo Uwkoh Zpyéwpb, Bgv bbr iwg gfwoorg xsfo gkoqh kxom pbhy Dcuéacx Zoxe, vpqodfr yo hrf Wcmobn ct uvs Hwoxu Dyléayo, kghsb o qsciz ct Hmobbrcc hfshusbfr k qmmmcbo igsou Nsouyo Fkhs. Gcib ct Bgv't Dyléayo, soqzeewbq Dwubqve, vke uyos wjggsou nvfwxh hrf qrbcg, pid hvoz tyvbr uvsss kkz pkdy dp vsn hrf tymzcgjbu eom. Ogr obn hvo chrffg uccu o pfffi poml hy hvo aosozoxe tbpa Dfbhkdccv & Dfbhkdfiom hy Hvo Uvyth yg Akjrsx'g Zfoy. Aoxz cp Ogr'g pjfgd Umw Ponhsg xsfo uwffb dp vsn aosszi cid ct hfodjhinf tys vst octwgdbbqo, fkuvsb hvko qvbwasou ko omuiov jwmucfi wb cohdms ywsf uvs Hma Msonffg. Ct uvs gwfcu tsws Qza Lbruot hrbh rbr lfsb hwjoo hy Ogr wb Lobdp, yozm Mh. Tifqf'g Uvixesf Corqf wx Szodhfsd Grpqy Tvcgeckx koc sobosr gfcw o fjqhysm so pkuhzo. Oci'g ufceczsc ksbf byu akes kom obgwos krfb rjg Miofwbbros, kghsb sjymjwxh wxuc k Qvksasvfcb, obn hvoo o Dvobjnobe, qssk ucc qckostiv obn fspvgsn hc mwgdfb dp vsn ixuwz Bgv dcive dbpjs iwacfzt. Cjos hsns, Bgv't roewqkuwcx hc iwg hcov dose cpg. Kghsb sobowbq vwc twbth ojuvd Ponhsg, Ogr ksxu cx hc dcazfhs jb dis Sorwqp Dvbhskv Qyotsbfbqo, onwobmjbu uc dis pjthr fceor - cbo tibuvsb hvko Uksm - obn sbnjbu vd so hrf Hyq 16. Tymzcgjbu Bgv't rogsod wb uvs Jbrshc Vfouef, Zsctotgcb Cou ogufr rja dp hbbjsv hc Wozooqwk Wgvbbr jb dis Ysobqf Obdvwzfzoqp hy fsdswsff hrf UC Povm tbpa k Dcuéacx Dfygsgcpf xbasn Wji. Krjzs jb dis Ysobqf Obdvwzfzoqp, Ktv vfofxfr yg hrf Cbbbuo Qfox, ko svjhs hfceq cp Hfkjbsbt byu ixmwyo Umw Zskesfc, hrbh zscjsesr dvovmsbqfg ppf Dsowxffg uc ywsfmpas jb dis Ysobqf Zobuio. Opusf Cfcml rodwroe hy ghkz ksuv Zsctotgcb Wji obn vsvq vos ksuv rff gpfy, Ogr obn Awcum gffs kcwxfr lz ox sbdiigsbghsd Dyléayo kkuqvos bknsr Ufomfm Clshmiwh. Ogr, Asthm, obn Hfkdsm ufofffgoe hrf Cbbbuo Wgvbbrc cb uvs coqu ct b zyth Vbdfkt. Ktv mbiuru hrf Zkqfoc ribjbu uvwc hwwf. Rf sffbhebzzi rspfohoe hrf tyvf wfaposg yg hrf Cbbbuo Qfox oxe tsoozvz kyo hrf Cbbbuo Zskhis cm dswiwqvwxh cfff dis Ysobqf Qbfk Cvdfons Qza Vforos, Nsoyo. Pogcfo zskwwbq hvo Cfkous Bfqrjdsvbuc, Ogr aoxbusn hc gwbn vwc Zozsog't dye oxe fomsocfr su pkdy sohc uvs xwzn. Izpb bfhibowbq hc Qozvfh Dpkb, Ogr rwcdcjossr uvod Pfydy rbr bfhibosr eis uc cpas vbyxpkb jbqsesbd hvku zogh dis ktdwbjbu Qcyéncb Cfsoesf jb nsson ct fjsx vskswbq Dfygsgcpf Swm'c bowf. Wfobgiwzo, Hbbqsi ghkzsr bh Zsctotgcb Cou'g vbpcbbhcbz oc Dfygsgcpf Yby\'t octwgdbbh, obn Pfydy bfxcsosr Bgv bbr Nwgdz. Kghsb vwc jwmucfi wb uvs Pfoxhs Vfouef, Ktv ooqceohsbfr Qbfm buoso, kor kghsb pssou nftskusr jb k cbo-cb-pbs cohdms, is vfofxfr dioh iwg swjkm vke pofb dsowxjbu iofn hc qofdjqwzbhs jb dis Cjzjos Qyotsbfbqo wb Kcvdp. Oyqwdfr ku hrf dbpgdodh yg aofhwxh box Dyléayo oxe tkdwbq Uobz oqbwb, Ogr obn tfsfbrc zspu tys hrf Xyihc ssuspb dp qymzsmu sshvh Corqfg psca uvs Hma Msonffg jb dis Tpvhy fsqjcb bbr dcazfhs jb dis Tpvhy Zskhis. Aimi zsls rjg dsojomg difcehv Ubbhy, Oci, Wjghi, oxe Pbpqy jadkdhsn hvo aoxz dopdzo obn Dcuéacx hvoz aou ovpbu uvs xom (cbo ct uvsw pssou Uvfh, hc xvcw Ogr rsvjjsbfr dis QT Pkmz). Wb Uvs Hfoct Fyvhs, Ogr qcwqshoe wx hvo Ufktg Dpifxbasxu ksuv Lvzpktoib. Vo zccu wx hvo twxbz bpibn hc Fdvbbwa\'g Clwdvpca. Ogr, ovpbu xwhr Awcum, dcazfhsn wb uvs Xvwbm Qeq wx hvo Kvssz Stzoxeg, xwhr Ogr dzkdwbq wb uvs Ucd 16. Is gbg nftskusr cm Wjghi wb b 3-pb-3; Asthm fbroe iz zccjbu jb dis xflh scixe. Ktv, bzcxh ksuv Bjhqrjs, uvsx vsvqsr tojo Gwvwsf, o miwzn Ziqjo, bbr jhg qofooh, gfcw hvo sjsm akdvwxbhwyog yg Hoba Bpqyou. Ktv ooqceohsbfr Qbfm pqqktwcxbzzi wb Kcvdp, kor, xvwvf hrfm gffs thwvm fswozc, Uksm cfsaoe hy jwox Oci ksuv cmwuruzm ncfo fscqsqd hvko pogcfo. Oci\'c ufyxhv pt miofkdhsb koc sjsesbd hvbpiurpih uvwc ghysm ksq kt vo qcxuwbefr dp hrjby ncfo obn acbf olpih iwg Qcyéncb bbr msgc obn zsct olpih iwacfzt. Otdff obfbsou ojuvd Xcruc Qza Lbruot, Ktv oohsbfr dis Cjzjos Qyotsbfbqo kwdi Uksm. Gwbkmzm, hvo hky fwfbzg cohdmsr jb dis dpifxbasxu wx o Pvzz Cohdms. Bzhrpiur wh xog b qvpgs cohdms, Bgv fasbhsr wwqdpfwyvg khowxth rjg bjjov tcb hvo twbth djas jb rjg mbfsos oc o Zpyéwpb Dsowxff, nofujbu pbs pt dis ljuuoth dvfbsou zpwbdt wx gose qksssb. Opusf uvs cohdms, Hofi obn Ogr rszbfhoe cx ucye hosag xwhr somi cdisf, twxbzzi cjosqcwjbu uvsss pyzvcye oxjaccjhm bbr ssqyhbwjjbu foqr chrff kt savozc. Oci\'c xcesbsi hvbpiur Yoxuc kor Tpvhy qowf hy ob fbr xvsx vs xog gcfmfr dp gzmwh vd gjhv Cfcml oxe Asthm xvsx hvo hky ct uvsw fsmfwjoe aotgoqfg dp fouifx vcwf.".lower()
+alphabet='abcdefghijklmnopqrstuvwxyz'
+blocks=[]
+
+frequency={'e':12.702,'t':9.356,'a':8.167,'o':7.507,'i':6.966,'n':6.749,'s':6.327,'r':5.987,'h':6.094,'d':4.253,'l':4.025,'u':2.758,'c':2.202,'m':2.406,'f':2.228,'y':1.994,'w':2.560,'g':2.015,'p':1.929,'b':1.492,'v':0.978,'k':1.292,'x':0.150,'q':0.095,'j':0.153,'z':0.077}
+
+i=0
+while i<assume:
+	blocks.append(ciphertext[i::assume])
+	i+=1
+
+for x in blocks:
+	total=0
+	count={'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0}
+	probabilty={0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0,19:0,20:0,21:0,22:0,23:0,24:0,25:0}
+	
+	for y in x:
+		if y in alphabet:
+			count[y]+=1
+			total+=1
+
+	for z in alphabet:
+		count[z]=count[z]/total
+
+	i=0
+	while i<26:
+		product=0
+		for w in alphabet:
+			product+=count[w]*frequency[w]
+
+		probabilty[i]=product
+		alphabet=alphabet[1:]+alphabet[0]
+		i+=1
+
+	probabilty = sorted(probabilty.items(), key=operator.itemgetter(1),reverse=True)
+	#print(probabilty)
+
+	
+	print(chr(probabilty[0][0]+97),end='')
+
+
+
+
+
+#print(blocks)
